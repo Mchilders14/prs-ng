ace7056bc6440922e0861dd1672a65ad61063384
mchilders14 <childersmatt4@gmail.com>
2021-08-17 01:44:36 -0400

    adding more for prs

--- src/app/feature/line-item/line-item-edit/line-item-edit.component.ts
+++ src/app/feature/line-item/line-item-edit/line-item-edit.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LineItem } from 'src/app/model/line-item.class';
+import { Product } from 'src/app/model/product.class';
+import { Request } from 'src/app/model/request.class';
+import { LineItemService } from 'src/app/service/line-item.service';
+import { ProductService } from 'src/app/service/product.service';
+import { RequestService } from 'src/app/service/request.service';
 
 @Component({
   selector: 'app-line-item-edit',
@@ -7,9 +14,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LineItemEditComponent implements OnInit {
 
-  constructor() { }
+  title: string = "Line-Item-Create";
+  lineItem: LineItem = new LineItem();
+  products: Product[] = [];
+  request: Request = new Request();
+  submitBtnTitle: string = 'Edit';
+  lineItemId: number = 0;
+
+  constructor( // Injecting Services
+    private lineItemSvc: LineItemService,
+    private productSvc: ProductService,
+    private requestSvc: RequestService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    // get line-item to edit
+    this.route.params.subscribe(parms => this.lineItemId = parms["id"]);
+    this.lineItemSvc.get(this.lineItemId).subscribe(
+      res => {
+        this.lineItem = res as LineItem;
+      },
+      err => { console.log(err); }
+    );
+
+     // populate list of products
+     this.productSvc.list().subscribe(
+      res => {
+        this.products = res as Product[]; 
+        console.log("List of Products: ", this.products); 
+      },
+      err => { 
+        console.log(err); 
+      }
+    );
   }
 
+  save() {
+    this.lineItemSvc.edit(this.lineItem).subscribe(
+      res => {
+        this.lineItem = res as LineItem;
+        this.router.navigateByUrl("/request-lines/" + this.requestSvc.currentRequest.id);
+      },
+      err => { console.log(err); }
+    );
+  }
+
+  compLineItem(a: LineItem, b: LineItem): boolean {
+    return a && b && a.id === b.id;
+  }
 }
