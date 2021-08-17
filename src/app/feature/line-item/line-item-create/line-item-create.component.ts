48b5a94aa56ed6c2666e82f429ac0ef66316a12e
mchilders14 <childersmatt4@gmail.com>
2021-08-16 22:48:16 -0400

    adding more for prs

--- src/app/feature/line-item/line-item-create/line-item-create.component.ts
+++ src/app/feature/line-item/line-item-create/line-item-create.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LineItemCreateComponent implements OnInit {
 
-  constructor() { }
+  title: string = "Product-Create";
+  product: Product = new Product();
+  vendors: Vendor[] = [];
+  submitBtnTitle: string = 'Create';
+
+  constructor(
+    private productSvc: ProductService,
+    private vendorSvc: VendorService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    // populate list of vendors
+    this.vendorSvc.list().subscribe(
+      res => {
+                this.vendors = res as Vendor[]; 
+                console.log("List of Vendors: ", this.vendors); 
+              },
+      err => { 
+                console.log(err); 
+              }
+    );
+  }
+
+  save() {
+    this.productSvc.create(this.product).subscribe(
+      res => {
+        this.product = res as Product;
+        this.router.navigateByUrl("/product-list");
+      },
+      err => { console.log(err); }
+    );
   }
 
 }
